7843afc2911e63b78a86d0f41bdacf3d611f8894
deriz13 <dederizki30102@gmail.com>
2023-02-25 21:14:25 +0700

    fix update qty process_order subtotal update otomatis

--- resources/views/processorder/index.blade.php
+++ resources/views/processorder/index.blade.php
@@ -25,7 +25,6 @@
 								<tr>
 									<td>PO ID</td>
 									<td>{{ $requestorder->request_id }}</td>
-									<input type="hidden" name="request_id" class="qty_update" value="">
 								</tr> 
 								<tr>
 									<td>Request Date</td>
@@ -65,50 +64,50 @@
 											<th>Info</th>
 											<th>Size / Weight</th>
 											<th>Approved
-												<label><input class="check-all" type="checkbox" onclick="$('input[name*=\'approved\']').prop('checked', this.checked);" checked></label>
+												<label><input class="check-all" type="checkbox" onclick="$('input[name*=\'approved\']').prop('checked', this.checked);"></label>
 											</th>
 											<th>Price (IDR)</th>
 											<th>Subtotal</th>
 										</tr>
 										@foreach($preorders as $p)
-											<tr>
-												<td align="center">
-													<input type="number" name="qty" class="po_qty" value="{{ $p->qty }}" min="1" class="quantity">
-												</td>
-												<td><a href="{{ $p->product_url }}" target="_blank">Link</a></td>
-												<td class="product-name"><p class="product-name" style="margin: 0;">{{ $p->product_name }}</p><br></td>
-												<td><p class="product-name" style="margin: 0;">{{ $p->color }}</p></td>
-												<td><p class="product-name" style="margin: 0;">@switch($p->status)
-														@case('00')
-															Pending Admin Verification
-															@break
-														@case('01')
-															Pending Your Approval
-															@break
-														@case('02')
-															Processed
-															@break
-														@case('03')
-															Rejected
-															@break
-														@default
-															Unknown Status
-													@endswitch</p>
-												</td>
-												{{var_dump($errors)}}
-												<td align="center">{{ $p->size }}</td>
-												<td align="center"><input type="checkbox" name="approved[{{$p->id}}]" value="{{$p->id}}" class="po_approved" checked></td>
-												<td align="right">{{ $p->price_customer }}</td>
-												<td align="right">{{ $p->qty * $p->price_customer }}</td>
-												<input type="hidden" id='subtotal{{$p->id}}' name="subtotal" class="po_subtotal" value="{{$p->qty * $p->price_customer}}">
-												@if ($errors->has('subtotal')) <small class="form-text help-block" style="color:red">{{ $errors->first('subtotal') }}</small> @endif
-											</tr>
-											<tr class="cart_total_price">
-												<td align="left" colspan="4" style="border-top:1px solid #ddd"></td>
-												<td colspan="4" align="right" style="font-weight:bold;border-top:1px solid #ddd"><strong>Total</strong></td>
-												<td align="right" style="border-top:1px solid #ddd"><span id="total{{$p->id}}">{{$p->qty * $p->price_customer}}</span></td>
-											</tr>
-										@endforeach										
+  <tr>
+    <td align="center">
+      <input type="number" name="qty" class="po_qty" value="{{ $p->qty }}" min="0" class="quantity" data-price="{{ $p->price_customer }}" data-id="{{ $p->id }}">
+    </td>
+    <td><a href="{{ $p->product_url }}" target="_blank">Link</a></td>
+    <td class="product-name"><p class="product-name" style="margin: 0;">{{ $p->product_name }}</p><br></td>
+    <td><p class="product-name" style="margin: 0;">{{ $p->color }}</p></td>
+    <td>
+      <p class="product-name" style="margin: 0;">
+        @switch($p->status)
+          @case('00')
+            Pending Admin Verification
+            @break
+          @case('01')
+            Pending Your Approval
+            @break
+          @case('02')
+            Processed
+            @break
+          @case('03')
+            Rejected
+            @break
+          @default
+            Unknown Status
+        @endswitch
+      </p>
+    </td>
+    <td align="center">{{ $p->size }}</td>
+    <td align="center"><input type="checkbox" name="approved" value="{{$p->id}}" class="po_approved"></td>
+    <td align="right">{{ $p->price_customer }}</td>
+    <td align="right" class="subtotal" id="total{{$p->id}}">{{ $p->qty * $p->price_customer }}</td>
+  </tr>
+  <tr class="cart_total_price">
+  <td align="left" colspan="4" style="border-top:1px solid #ddd"></td>
+  <td colspan="4" align="right" style="font-weight:bold;border-top:1px solid #ddd"><strong>Total</strong></td>
+  <td align="right" style="border-top:1px solid #ddd"><span id="grand_total"></span></td>
+</tr>
+@endforeach			
 									</table>
 									<strong>Note : {{ $requestorder->note }} </strong>
 								</div>
@@ -119,8 +118,6 @@
   				</div>
   				<div id="tabs-2">
     				<div class="row"><div class="col-lg-12">
-						<input type="hidden" name="orderno" value="WX19030396">
-						<input type="hidden" name="status" value="2"><div class="block-dropshipper">
 						<div id="dropshipper">
 							<div class="row">
 								<div class="col-sm-6">
@@ -149,7 +146,6 @@
 															<option value="">-Select Province-</option>
 															<option  value="1">Bali</option>
 														</select>
-														<input type="hidden" id="nama_propinsi" name="nama_propinsi" value='Jawa Barat'>
 													</div>
 												</div>
 												<div class="form-group">
@@ -159,7 +155,6 @@
 															<option value="">-Select City-</option>
 															<option  value="23">Bandung</option>													
 														</select>
-														<input type="hidden" id="nama_kota" name="nama_kota">
 													</div>
 												</div>
 												<div class="form-group">
@@ -169,7 +164,6 @@
 															<option value="">-Select Kecamatan-</option>
 															<option  value="23">Cilawu</option>				
 														</select>
-														<input type="hidden" id="nama_kecamatan" name="nama_kecamatan">
 													</div>
 												</div>		
 												<div class="form-group">
@@ -247,7 +241,6 @@
 														<select  name="kecamatan_dropship" id="kecamatan_dropship" class="form-control"   = "" >
 															<option value="">-Select Kecamatan-</option>								
 								 						</select>
-														<input type="hidden" id="nama_kecamatan_dropship" name="nama_kecamatan_dropship">
 													</div>
 												</div>					
 												<div class="form-group">
@@ -319,8 +312,6 @@
 														<div class="col-sm-8 col-md-8">
 															<span id='delivery_fee_summary'> 0</span> &nbsp;<span id="delivery_fee_desc"></span>
 														</div>
-														<input type="hidden" name='delivery_fee_id_summary' id='delivery_fee_id_summary' value='0'>
-														<input type="hidden" name='delivery_fee_description' id='delivery_fee_description' value=''>
 													</div>
 													<div class="form-group">
 														<div class="col-sm-4 col-md-3 text-right">Insurance</div>
@@ -357,7 +348,6 @@
 																<div class="col-sm-8 col-md-8">
 																	<span id='total_outstanding'><strong>1,910,320</strong></span>
 																</div>
-																<input type='hidden' name='total_outstanding' id='total_outstanding2' value='0'>
 															</div>						
 													</fieldset>
 												</div>
@@ -371,31 +361,21 @@
 										<img id="loader" class="mfp-hidex" src="https://www.myhouseofmakeup.co/images/ajax-loader.gif" alt="loader" style="display:none">
 										<div id="form-ongkir" class="hide_divx"></div>	<!-- /#form-ongkir --><strong>Total Weight</strong>: Asumption 1 Kg
 										<br/><br/>
-										<input type="hidden" name="district_valid" id="district-valid" value="0">
-										<input type="hidden" id="totalweight" value="1">
-										<input type="hidden" name="weight" id="weight" value="Asumsi 1 kg" />
-										<input type="hidden" name="postagehidden" id="postagehidden" />
-										<input type="hidden" name="cityhidden" id="cityhidden" />
-										<input type="hidden" name="postagecity" id="postagecity" />
-										<input type="hidden" name="postageday" id="postageday" />
-										<input type="hidden" name="json_courier" id="json_courier" />
 									</div>
 									<div class="col-sm-6 col-md-4"><h4>Additional</h4>
 										<table class="addditional-cart"><tr class="cart_total_price"><td class="use_insurance_container text-right"><span>Insurance</span>
 											<label>
 												<input type="checkbox" name="isurance" id="use_insurance" value="1" ><span class="checkbox-material"><span class="check"></span></span>
-											</label></td><td></td><td id="use_insurance_container" class="insurance"><input type="hidden" name="insurance_value" id="insurance_value" value="9668.32"><span id="insurance_pricessssssssss"></span></td></tr><tr class="cart_total_price"><td class="use_packing_container text-right"><span>Block Packing</span>
+											</label></td><td></td><td id="use_insurance_container" class="insurance"><span id="insurance_pricessssssssss"></span></td></tr><tr class="cart_total_price"><td class="use_packing_container text-right"><span>Block Packing</span>
 											<label>
 												<input type="checkbox" name="use_packing" id="use_packing" value="1"  disabled="">
 												<span class="checkbox-material"><span class="check"></span></span>
-												<input type="hidden" id="packing_value" name="packing_value" value="">
-												<input type="hidden" id="tempCity" name="tempCity">
-											</label></td><td> </td><td id="use_packing_container" class="packing"><input type="hidden" name="result_packing" id="result_packing" value=""><span id="packing_pricesssssssssssssss"></span></td></tr>
+											</label></td><td> </td><td id="use_packing_container" class="packing"><span id="packing_pricesssssssssssssss"></span></td></tr>
 											<tr class="cart_total_price">
 												<td class="use_insurance_container text-right"><span>Use E-Wallet</span>
 												<label>
 													<input type="checkbox" readonly name="use_ewallet" id="use_ewallet" value="1" ><span class="checkbox-material"><span class="check"></span></span>
-												</label></td><td></td><td id="use_ewallet" class="insurance"><input type="hidden" name="ewallet_value" id="ewallet_value" value="1000000"><span id="ewallet_value_span"></span></td>
+												</label></td><td></td><td id="use_ewallet" class="insurance"><span id="ewallet_value_span"></span></td>
 											</tr>
 										</table>
 									</div>	</div>	<h4>Notes</h4>
@@ -423,11 +403,29 @@
 	</div>
 @endsection
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-function myFunction() {
-  if (confirm("Are you sure you want to process this transaction?")) {
-		document.getElementById("checkout-form2").submit();
-  } else {
-  }
-}
+$(document).ready(function() {
+  // Hitung total subtotal dan tampilkan
+  var subtotal = 0;
+  $('.subtotal').each(function() {
+    subtotal += parseInt($(this).text());
+  });
+  $('#grand_total').text(subtotal);
+});
+
+$(document).on('input', '.po_qty', function() {
+  // Hitung ulang subtotal dan total keseluruhan saat input diubah
+  var qty = $(this).val();
+  var price = $(this).data('price');
+  var total = qty * price;
+  var id = $(this).data('id');
+  $('#total' + id).text(total);
+
+  var subtotal = 0;
+  $('.subtotal').each(function() {
+    subtotal += parseInt($(this).text());
+  });
+  $('#grand_total').text(subtotal);
+});
 </script>
