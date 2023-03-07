ae923670b9b0945665ccfd42c72fd49c87d909ba
budiyanaa <asepbudiyana12@gmail.com>
2023-03-07 23:17:54 +0700

    fix input note

--- app/Http/Controllers/PreOrderController.php
+++ app/Http/Controllers/PreOrderController.php
@@ -144,6 +144,7 @@ class PreOrderController extends Controller
 
                 'POUUID' => "",
                 'InvoiceUUID' => "",
+                'note' => "",
             ]);
             
             // $id= uniqid();
