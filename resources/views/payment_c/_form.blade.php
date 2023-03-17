4858f3f06b4d096b7a6991830d0f2611b862c556
deriz13 <dederizki30102@gmail.com>
2023-03-17 14:43:44 +0700

    fix preorder list and tambag tanda koma

--- resources/views/payment_c/_form.blade.php
+++ resources/views/payment_c/_form.blade.php
@@ -21,23 +21,23 @@
 		{{ Form::select('bank_id', $banks, null, ['class' => 'form-control',  'placeholder' => '--Select Bank--']); }}
 		@if ($errors->has('bank_destination')) <small class="form-text help-block" style="color:red">{{ $errors->first('bank_destination') }}</small> @endif
 	</div>
-    <div class="form-group 	@if ($errors->has('payment_date')) has-error @endif col-md-6 col-lg-12">
-	<span class="text-right req">Payment Date<span class="reqsign">*</span></span>
-		{{ Form::date('payment_date', null, ['class' => 'form-control', 'placeholder' => 'Payment Date']) }}
-		@if ($errors->has('payment_date')) <small class="form-text help-block" style="color:red">{{ $errors->first('payment_date') }}</small> @endif
-	</div>
+    <div class="form-group @if ($errors->has('payment_date')) has-error @endif col-md-6 col-lg-12">
+    <span class="text-right req">Payment Date<span class="reqsign">*</span></span>
+    {{ Form::text('payment_date', null, ['id' => 'datepicker', 'class' => 'form-control', 'placeholder' => 'Payment Date']) }}
+    @if ($errors->has('payment_date')) <small class="form-text help-block" style="color:red">{{ $errors->first('payment_date') }}</small> @endif
+</div>
     <div class="form-group 	@if ($errors->has('bank_name')) has-error @endif col-md-6 col-lg-12">
 	<span class="text-right req">Your Bank name<span class="reqsign">*</span></span>
 		{{ Form::text('source_bank_name', null, ['class' => 'form-control', 'placeholder' => 'Bank Name']) }}
 		@if ($errors->has('bank_name')) <small class="form-text help-block" style="color:red">{{ $errors->first('bank_name') }}</small> @endif
 	</div>
     <div class="form-group 	@if ($errors->has('bank_account')) has-error @endif col-md-6 col-lg-12">
-	<span class="text-right req">Your Bank Account No*<span class="reqsign">*</span></span>
+	<span class="text-right req">Your Bank Account No<span class="reqsign">*</span></span>
 		{{ Form::number('source_acct_no', null, ['class' => 'form-control', 'placeholder' => 'Bank Account']) }}
 		@if ($errors->has('bank_account')) <small class="form-text help-block" style="color:red">{{ $errors->first('bank_account') }}</small> @endif
 	</div>
     <div class="form-group 	@if ($errors->has('account_name')) has-error @endif col-md-6 col-lg-12">
-	<span class="text-right req">Your Account Name*<span class="reqsign">*</span></span>
+	<span class="text-right req">Your Account Name<span class="reqsign">*</span></span>
 		{{ Form::text('source_acct_name', null, ['class' => 'form-control', 'placeholder' => 'Account Name']) }}
 		@if ($errors->has('account_name')) <small class="form-text help-block" style="color:red">{{ $errors->first('account_name') }}</small> @endif
 	</div>
@@ -87,5 +87,12 @@
 		var symbol = '';
 		return accounting.formatMoney(str, symbol, 0, ".", ",");
 	}	
+
+	$(function() {
+        $("#datepicker").datepicker({
+            dateFormat: "yy-mm-dd"
+        });
+    });
+
 </script>
 @endsection
