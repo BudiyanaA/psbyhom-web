b3488c631ca64778d49700cd5504e6b7c39a3d46
budiyanaa <asepbudiyana12@gmail.com>
2023-03-29 06:50:09 +0700

    fix update notes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 			<div id="slide-home" class="owl-carousel owl-theme">
 				<div class="item"><a href="#" target="_blank"><img src="{{ url('assets/img/Slide1.jpg') }}" alt="" /></a></div>
 				<div class="item"><a href="#" target="_blank"><img src="{{ url('assets/img/Slide2.jpg') }}" alt="" /></a></div>
-				<div class="item"><a href="#" target="_blank"><img src="{{ url('assets/img/slidemaintenance.png') }}" alt="" /></a></div>							
+				<!-- <div class="item"><a href="#" target="_blank"><img src="{{ url('assets/img/slidemaintenance.png') }}" alt="" /></a></div>							 -->
 			</div>
 		</div>
 	</div>
