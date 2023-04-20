466926337667ea805f1476150ea0da450a4d99b1
Asep Budiyana M <asepbudiyana12@gmail.com>
2023-04-20 10:18:50 +0700

    fix update batch and search costumer

--- app/Http/Controllers/CostumerManagementController.php
+++ app/Http/Controllers/CostumerManagementController.php
@@ -11,7 +11,7 @@ class CostumerManagementController extends Controller
     {
         $data['costumer'] = Registercostumer::orderBy('created_date', 'DESC');
         if ($request->customer_name) {
-            $data['costumer'] = $data['costumer']->where('customer_name', 'like', $request->customer_name);
+            $data['costumer'] = $data['costumer']->where('customer_name', 'like', '%' . $request->customer_name . '%');
         }
         $data['costumer'] = $data['costumer']->paginate(10);
         return view('costumer.index',$data);
