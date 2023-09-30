0147109ccd97634d23a0621ae2a09ef742eaabb6
Asep Budiyana M <asepbudiyana12@gmail.com>
2023-09-30 10:30:13 +0700

    fix fe slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 		<div class="container slider">
 			<div id="slide-home" class="owl-carousel owl-theme">
 				@foreach($slides as $slide)
-					<div class="item"><a href="{{ $slide->remarks }}" target="_blank"><img src="{{ url('assets/' . $slide->image_slide ) }}" alt="{{ $slide->slide_name }}" /></a></div>
+					<div class="item"><a href="{{ $slide->ArticleUUID }}" target="_blank"><img src="{{ url('assets/' . $slide->image_slide ) }}" alt="{{ $slide->slide_name }}" /></a></div>
 				@endforeach
 			</div>
 		</div>
