dab9bfc20001094ce1127ac2ed9d95e8fbad7b60
Asep Budiyana M <asepbudiyana12@gmail.com>
2023-09-30 10:29:14 +0700

    fix fe slider

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MsFrontpageSlideshow;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $data['slides'] = MsFrontpageSlideshow::where('status', '01')
+            ->orderBy('seq', 'ASC')->get();
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
     <div class="index-space"></div>
 		<div class="container slider">
 			<div id="slide-home" class="owl-carousel owl-theme">
-				<div class="item"><a href="#" target="_blank"><img src="{{ url('assets/img/Slide1.jpg') }}" alt="" /></a></div>
-				<div class="item"><a href="#" target="_blank"><img src="{{ url('assets/img/Slide2.jpg') }}" alt="" /></a></div>
-				<!-- <div class="item"><a href="#" target="_blank"><img src="{{ url('assets/img/slidemaintenance.png') }}" alt="" /></a></div>							 -->
+				@foreach($slides as $slide)
+					<div class="item"><a href="{{ $slide->remarks }}" target="_blank"><img src="{{ url('assets/' . $slide->image_slide ) }}" alt="{{ $slide->slide_name }}" /></a></div>
+				@endforeach
 			</div>
 		</div>
 	</div>
